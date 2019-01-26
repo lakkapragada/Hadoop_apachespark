b28bfdb60193d80d422cf40a26317fe27cdd5a4b
lakkapragada <46225060+lakkapragada@users.noreply.github.com>
2019-01-26 17:06:43 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -2,6 +2,6 @@ from pyspark import SparkConf,SparkContext
 conf = SparkConf().setAppName('test spark')
 sc = SparkContext(conf = conf)
 
-for i in range(10,0,-1):
+for i in sc.parallelize(xrange(0,10,1),9).glom().collect():
     print ('hello everyone this is a test program')
     print('count down {}'.format(i))
