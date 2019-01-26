e70352e68f463cb05e0e12db245adcb0073cd01c
lakkapragada <46225060+lakkapragada@users.noreply.github.com>
2019-01-26 17:11:41 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -4,4 +4,4 @@ sc = SparkContext(conf = conf)
 
 for i in sc.parallelize(xrange(0,10,1),9).glom().collect():
     print ('hello everyone this is a test program')
-    print('count down {}'.format(i))
+    print('count down' ,i)
