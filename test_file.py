191625b77f796942be70934bba81589bd34ff21f
lakkapragada <46225060+lakkapragada@users.noreply.github.com>
2019-01-26 16:46:36 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,3 +1,7 @@
+from pyspark import SparkConf,SparkContext
+conf = SparkConf().setAppName('test spark')
+sc = SparkContext(conf = conf)
+
 for i in range(10,0,-1):
     print ('hello everyone this is a test program')
     print('count down {}'.format(i))
