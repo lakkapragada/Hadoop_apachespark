4422eea54e737edde62919f866f34d7b38592ddb
lakkapragada <46225060+lakkapragada@users.noreply.github.com>
2019-01-27 14:54:17 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -2,7 +2,7 @@ from pyspark import SparkConf,SparkContext
 conf = SparkConf().setAppName('test spark')
 sc = SparkContext(conf = conf)
 
-data = sc.textFile('hdfs:///user/maria_dev/u.data')
+data = sc.textFile('hdfs:///home/maria_dev/u.data')
 
 splitted = data.map(lambda x: x.split())
 
