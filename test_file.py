fcd67252f52da3f5e6138fe2f233f2f9c6c4d9f0
lakkapragada <46225060+lakkapragada@users.noreply.github.com>
2019-01-28 11:14:06 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -2,10 +2,13 @@ from pyspark import SparkConf,SparkContext
 conf = SparkConf().setAppName('test spark')
 sc = SparkContext(conf = conf)
 
+def convert(x):
+    return [int(x[0]),int(x[1]),float(x[2]),int(x[4])]
+
+
 data = sc.textFile('hdfs:///user/maria_dev/ml-100k/u.data')
 
-splitted = data.map(lambda x: x.split())
+out = data.map(lambda x:x.split())
+out.map(convert)
+out.collect()
 
-elements = splitted.collect()
-
-print(elements)
