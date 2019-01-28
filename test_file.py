c4362e824325fc701d19055c3c9a1c8b8de99d92
lakkapragada <46225060+lakkapragada@users.noreply.github.com>
2019-01-28 13:35:17 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -10,7 +10,7 @@ spark = SparkSession(sc)
 
 
 # take input data from hdfs file system
-data = spark.textFile('hdfs:///user/maria_dev/ml-100k/u.data')
+data = spark.sparkContext.textFile('hdfs:///user/maria_dev/ml-100k/u.data')
 
 # perform some transformations on RDD 
 out = data.map(lambda x:x.split()).map(lambda x:[int(x[0]),int(x[1]),float(x[2]),int(x[4])])
@@ -21,7 +21,7 @@ spark_df = out.toDF(['id','movie_ID','rating','time_stamp'])
 
 # movie names list is stored in another meta data file
 
-items = spark.textFile('hdfs:///user/maria_dev/ml-100k/u.item')
+items = spark.sparkContext.textFile('hdfs:///user/maria_dev/ml-100k/u.item')
 movie_items = items.map(lambda x:x.split('|')).map(lambda x:[int(x[0]),str(x[1])])
 movie_df = movie_items.toDF(['movie_ID','movie_name'])
 # average ratings of each movie
