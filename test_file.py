3e6946e27ed03229b142bfec7baf3eed158a9ad2
lakkapragada <46225060+lakkapragada@users.noreply.github.com>
2019-01-28 13:30:47 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,14 +1,16 @@
 # This program is to test the top movies accoriding ratings by users in pyspark frame work
 # a public data set was taken and stored in hadoop distribution 
 
-from pyspark import SparkConf,SparkContext
-from pyspark.sql import *
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
 conf = SparkConf().setAppName('test spark')
 sc = SparkContext(conf = conf)
+spark = SparkSession(sc)
+
 
 # take input data from hdfs file system
-data = sc.textFile('hdfs:///user/maria_dev/ml-100k/u.data')
+data = spark.textFile('hdfs:///user/maria_dev/ml-100k/u.data')
 
 # perform some transformations on RDD 
 out = data.map(lambda x:x.split()).map(lambda x:[int(x[0]),int(x[1]),float(x[2]),int(x[4])])
@@ -19,7 +21,7 @@ spark_df = out.toDF(['id','movie_ID','rating','time_stamp'])
 
 # movie names list is stored in another meta data file
 
-items = sc.textFile('hdfs:///user/maria_dev/ml-100k/u.item')
+items = spark.textFile('hdfs:///user/maria_dev/ml-100k/u.item')
 movie_items = items.map(lambda x:x.split('|')).map(lambda x:[int(x[0]),str(x[1])])
 movie_df = movie_items.toDF(['movie_ID','movie_name'])
 # average ratings of each movie
